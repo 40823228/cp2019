965d1a568d6e0e7d8ebdc1700f2e4d1c29ebd487
40823228 <lyndonly@gmail.com>
2019-10-24 15:18:08 +0800

    initial add.

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '\n \n int i;\nint sum;\n\nmain(){\n  sum = 0;\n  for(i=1;i <= 10 ;i++){\n    sum += i;\n    print("$sum");\n  }\n  print(\'sum = $sum\');\n} \n \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'First', 'text': 'Hello\xa0\xa0 程式碼     1->10\xa0\xa0 程式碼 \n \n int i;\nint sum;\n\nmain(){\n  sum = 0;\n  for(i=1;i <= 10 ;i++){\n    sum += i;\n    print("$sum");\n  }\n  print(\'sum = $sum\');\n} \n', 'tags': '', 'url': 'First.html'}]};
